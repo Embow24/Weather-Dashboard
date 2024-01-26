0067f1e5d725d54b50e52bc14cf3edf2f252e36d
Emily Kent <embow@Emilys-MacBook-Pro.local>
2024-01-26 20:16:59 +0000

    function added to fetch data from URL when search button is clicked

---
+++ script.js
@@ -0,0 +1,29 @@
+// API key //
+var APIKey = "9404079bc4b50a677177ee55266f7815"
+
+//function to fetch the current weather data when the search button is clicked
+$("#search-button").on("click", function(event) {
+
+event.preventDefault()
+
+//variable to get the text from the search box
+var city = $("#search-input").val()
+
+//URL to get the data from the open weather page
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey
+
+//function to fetch the data from the api webpage
+fetch(queryURL)
+.then(function (response) {
+    return response.json()
+})
+.then(function (data) {
+
+    console.log(queryURL)
+    console.log(data)
+})
+
+
+})
+
+
