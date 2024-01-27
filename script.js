a52f9df1f4f9a857eff1c481049cdda776aa5c39
Emily Kent <embow@Emilys-MacBook-Pro.local>
2024-01-27 14:04:02 +0000

    created div for the temp, humidity and wind and appended them to HTML file

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ event.preventDefault()
 //variable to get the text from the search box
 var city = $("#search-input").val()
 
-//URL to get the data from the open weather page
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey
+//URL to get the current weather data from the open weather page
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
 
 //function to fetch the data from the api webpage
 fetch(queryURL)
@@ -26,10 +26,24 @@ fetch(queryURL)
     console.log(data)
 
 //Displays the city name and current date
-$("#today").html("<h1>" + data.city.name + " (" + currentDate.format('DD/MM/YYYY') + ")" + "</h1>")    
+$("#today").html("<h1>" + data.name + " (" + currentDate.format('DD/MM/YYYY') + ")" + "</h1>")
+
+//create div to display the temperature of the current location
+var temp = $('<div>').text("Temp: " + data.main.temp)
+//create div to display the wind speed of the current location
+var wind = $('<div>').text("Wind: " + data.wind.speed + " KPH")
+//create div to display the humidity of the current location
+var humidity = $('<div>').text("Humidity: " + data.main.humidity + "%")
+
+//Add the temp, wind and humidity variable to the section with id = today in the html file
+$("#today").append(temp)
+$("#today").append(wind)
+$("#today").append(humidity)
 })
 
 
 })
 
 
+//URL to get the data from the open weather page
+//var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey
