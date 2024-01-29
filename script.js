1819a525cd9d429cb7566115e596ca860ac64e2b
Emily Kent <embow@Emilys-MacBook-Pro.local>
2024-01-29 12:58:45 +0000

    added weather icon URL

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ fetch(queryURL)
     console.log(data)
 
 //console.log(data.list[0].weather[0].icon)
-//var icon = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png"
+var icon = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png"
     
-//Displays the city name and current date
-$("#today").html("<h1>" + data.city.name + " (" + currentDate.format('DD/MM/YYYY') + ")" + "</h1>")
+//Displays the city name, current date and weather icon
+$("#today").html("<h1>" + data.city.name + " (" + currentDate.format('DD/MM/YYYY') + ")" + "<img src=" + icon +">" + "</h1>")
 //Change temperature to celsius
 var tempC = data.list[0].main.temp - 273.15
 
@@ -49,22 +49,33 @@ var card = [data.list[2], data.list[10], data.list[18], data.list[26], data.list
 
 //For loop to make 5 cards showing next 5 days weather forecast
 for (var i=0; i<card.length; i++) {
+
 //Change temperature to celsius
 var forecastTempC = card[i].main.temp - 273.15
+
+//variable for the icons needed for the next 5 days forecast
+var forecastIcon = "https://openweathermap.org/img/wn/" + card[i].weather[0].icon + "@2x.png"
 
 //create card for 5 day weather forecast
 var forecast = $('<div class= "card text-bg-secondary mb-3" style="max-width: 10rem">')
 var date = $('<h5 class="card-title">').text(card[i].dt_txt)
+var icons = $("<img src=" + forecastIcon +">")
 var temp = $('<p class="card-text">').text("Temp: " + forecastTempC.toFixed(2) + " C")
 var wind = $('<p class="card-text">').text("Wind: " + card[i].wind.speed + " KPH")
 var humidity = $('<p class="card-text">').text("Humidity: " + card[i].main.humidity + "%")
 
 $("#forecast").append(forecast)
 $(forecast).append(date)
+$(forecast).append(icons)
 $(forecast).append(temp)
 $(forecast).append(wind)
 $(forecast).append(humidity)
 }
+
+//Data saved to local storage when button clicked
+
+//Show data from local storage using buttons
+
 })
 })
 
